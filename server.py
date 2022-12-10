d474f42c044db70862d09eb86942d5700cdadf05
VFatekhova <kuminova_v@mail.ru>
2022-12-10 03:58:28 +0300

    Http reauest is added

--- server.py
+++ server.py
@@ -17,13 +17,43 @@ class TCPServer:
             s.listen(5)
             while True:
                 client, addr = s.accept()
-                res = client.recv(1024).decode("utf-8")
-                l = res.replace("\r", "").split("\n")
-                answer = ()
+                res = self.read_client_data(client)
+                http_response = (
+                    f"HTTP/1.0 200 OK\r\n"
+                    f"Server: otusdemo\r\n"
+                    f"Date: Sat, 01 Oct 2022 09:39:37 GMT\r\n"
+                    f"Content-Type: text/html; charset=UTF-8\r\n"
+                    f"\r\n"
+                )
+
                 print(res)
-                print(l)
-                client.send(res.encode())
+
+                client.send(http_response.encode())
                 client.close()
+
+    def read_client_data(self, client):
+        end_of_stream = '\r\n\r\n'
+        str_data = ""
+        while True:
+            data = client.recv(1024)
+            if not data:
+                break
+            str_data += data.decode("utf-8")
+            if end_of_stream in str_data:
+                break
+        l = str_data.replace("\r", "").split("\n")
+        return l
+
+    def create_http_responce(self, client_data):
+        http_response = (
+            f"HTTP/1.0 200 OK\r\n"
+            f"Server: otusdemo\r\n"
+            f"Date: Sat, 01 Oct 2022 09:39:37 GMT\r\n"
+            f"Content-Type: text/html; charset=UTF-8\r\n"
+            f"\r\n"
+        )
+        return http_response
+
 
 server = TCPServer()
 server.server_run("127.0.0.1", 8885)
