20e5dc5c338615f1d4689bda31f8a8917a24f5e4
VFatekhova <kuminova_v@mail.ru>
2022-12-13 17:51:50 +0300

    done

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from http import HTTPStatus
 class UDPServer:
     def server_run(self, host, port):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.bind((host, port))  # host = 127.0.0.1, port = 8885
+            s.bind((host, port))
             res = s.recv(1024)
             print("Message: ", res.decode("utf-8"))
             s.close()
@@ -37,42 +37,36 @@ class TCPServer:
         return l
 
     def create_http_responce(self, client_data):
-        query_params = "status=200"
         header = ""
         request_method = ""
         request_source = ""
-        status = 200
+        status_value = 200
+        status_phrase = "OK"
         try:
             query_params = client_data[0].split()[1][2:].split("&")
-            print(query_params)
             request_method = client_data[0].split()[0]
             for param in query_params:
                 if param[0:6] == "status":
-                    status = int(param.split("=")[1])
-            if 99 < status < 600:
-                pass
-            else:
-                status = 200
-
+                    status_value = int(param.split("=")[1])
+                    status_phrase = HTTPStatus(status_value).phrase
         except:
-            pass
-        if HTTPStatus.value == 200:
-            print(HTTPStatus.phrase)
+            status_value = 200
+            status_phrase = "OK"
+
         for param in client_data[1:]:
             if param[0:4] == "Host":
                 request_source = param.split()[1]
         header += f"Request Method: {request_method}\r\n"
         header += f"Request Source: {request_source}\r\n"
-        header += f"Response Status: {status}\r\n"
+        header += f"Response Status: {status_value}\r\n"
         for param in client_data[1:]:
             header += f"{param}\r\n"
 
         http_response = (
-            f"HTTP/1.0 200 OK\r\n"
+            f"HTTP/1.0 {status_value} {status_phrase}\r\n"
             f"{str(header)}\r\n"
             f"\r\n"
         )
-        print(http_response)
         return http_response
 
 
